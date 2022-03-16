3c01484837fc1fd54cc154b27112f3eb24ced90e
Alex Tyler Chapman <chapman39@llnl.gov>
2022-03-16 12:14:42 -0700

    added more baselines and created ats generator

---
+++ integration/add_ats.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+ORIG_DIR=$(pwd) # should be ./serac_repo/tests/integration
+
+# for each type,
+TYPES=(meshing solid thermal_solid thermal_conduction)
+for _type in ${TYPES[@]}
+    do
+
+    # for each test in directory "t"
+    cd "./${_type}"
+    for t in $(ls --color=no) 
+        do
+
+        echo "Writing ats file for ${_type}/${t}..."
+
+        # set dir for new ats file with the name: "t".ats
+        ATS_FILE="${t}/${t}.ats"
+
+        # insert the following text
+        NAME_SERIAL="${t}_serial"
+        NAME_PARALLEL="${t}_parallel"
+        INPUT_FILE="{0}/tests/${_type}/${t}.lua"
+
+        echo "##############################################################################" > ${ATS_FILE}
+        echo "# Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and" >> ${ATS_FILE}
+        echo "# other Serac Project Developers. See the top-level COPYRIGHT file for details." >> ${ATS_FILE}
+        echo "#" >> ${ATS_FILE}
+        echo "# SPDX-License-Identifier: (BSD-3-Clause)" >> ${ATS_FILE}
+        echo "##############################################################################" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+        echo "tolerance_test(name=\"${NAME_SERIAL}\"," >> ${ATS_FILE}
+        echo "    input_file=\"${INPUT_FILE}\".format(serac_input_files_dir))" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+        echo "tolerance_test(name=\"${NAME_PARALLEL}\"," >> ${ATS_FILE}
+        echo "    input_file=\"${INPUT_FILE}\".format(serac_input_files_dir)," >> ${ATS_FILE}
+        echo "    num_mpi_tasks=2)" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+
+    done
+
+    cd ${ORIG_DIR}
+done
+
