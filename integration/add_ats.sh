2b9c47227152acb391ca67e688035a6f64497556
Alex Tyler Chapman <chapman39@llnl.gov>
2022-03-22 15:38:27 -0700

    added baselines and ignoring serac output

---
+++ integration/add_ats.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+# desc: generates new ats files for each test based off input file structure
+
+ORIG_DIR=$(pwd) # should be ./serac_repo/tests/integration
+INPUT_DIR="${ORIG_DIR}/../../data/input_files/tests/"
+
+# for each type,
+TYPES=(solid thermal_solid thermal_conduction)
+
+cd ${INPUT_DIR}
+
+for _type in ${TYPES[@]} ; do
+
+    cd "./${_type}"
+
+    # for each test in directory "t"
+    for t in $(ls --color=no) ; do
+
+        # remove .lua from file name
+        t=$(echo $t | awk -F "." '{print $1}')
+
+        echo "Writing ats file for ${_type}/${t}..."
+
+        cd ${ORIG_DIR}
+
+        if [ ! -d ${_type}/${t} ] ; then
+            mkdir -vp ${_type}/${t}
+        fi
+
+        cd ${_type}
+
+        ATS_FILE="${t}/${t}.ats"
+
+        # insert the following text
+        NAME_SERIAL="${t}_serial"
+        NAME_PARALLEL="${t}_parallel"
+        INPUT_FILE="{0}/tests/${_type}/${t}.lua"
+
+        echo "##############################################################################" > ${ATS_FILE}
+        echo "# Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and" >> ${ATS_FILE}
+        echo "# other Serac Project Developers. See the top-level COPYRIGHT file for details." >> ${ATS_FILE}
+        echo "#" >> ${ATS_FILE}
+        echo "# SPDX-License-Identifier: (BSD-3-Clause)" >> ${ATS_FILE}
+        echo "##############################################################################" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+        echo "tolerance_test(name=\"${NAME_SERIAL}\"," >> ${ATS_FILE}
+        echo "    input_file=\"${INPUT_FILE}\".format(serac_input_files_dir))" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+        echo "tolerance_test(name=\"${NAME_PARALLEL}\"," >> ${ATS_FILE}
+        echo "    input_file=\"${INPUT_FILE}\".format(serac_input_files_dir)," >> ${ATS_FILE}
+        echo "    num_mpi_tasks=2)" >> ${ATS_FILE}
+        echo "" >> ${ATS_FILE}
+
+        cd ${INPUT_DIR}
+
+    done
+
+done
+
